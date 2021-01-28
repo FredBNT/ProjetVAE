684158e851fdf3e85c0acb2028ae5547a99b465d
fred_ <fred_@HERA.mshome.net>
2021-01-28 13:59:40 +0100

    update9

---
+++ WebContent/WEB-INF/jsp/PagePrincipale.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!-- vro/page principale -->
+
+<!DOCTYPE html>
+<title>Main Page</title>
+<html>
+<link rel="shortcut icon" type="image/png" href="./lib/iconeLogo.jpg" />
+<%@include file="integrationStyle.jsp"%>
+
+<jsp:include page="/WEB-INF/fragments/head.jsp"></jsp:include>
+
+<h1> en cours</h1>
+
+
+<jsp:include page="/WEB-INF/fragments/footer.jsp"></jsp:include>
+</body>
+
+</html>
---
+++ src/fr/eni/vae/servlets/ServPagePrincipale.java
@@ -0,0 +1,36 @@
+package fr.eni.vae.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServPagePrincipale
+ */
+@WebServlet("/ServPagePrincipale")
+public class ServPagePrincipale extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		if(request.getParameter("successCreateVente") != null && request.getParameter("successCreateVente").equals("1")) {
+			
+			request.setAttribute("MessageAjoutVente", "Votre annonce vient d'etre publiee");
+		}
+		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/PagePrincipale.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
